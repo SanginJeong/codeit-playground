255eb6b8347a59a07a0a7b81998ad24e2ddf0919
sanginjeong <sanginjeong07@gmail.com>
2025-07-30 15:03:15 +0900

    feat: routes setting

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,24 @@
 import "./App.css";
-
+import { Routes, Route } from "react-router";
+import AppLayout from "./layout/AppLayout";
+import AdminPage from "./pages/AdminPage/AdminPage";
+import HomePage from "./pages/HomePage/HomePage";
+import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
+import PrivatePage from "./pages/PrivatePage/PrivatePage";
 function App() {
-  return <></>;
+  return (
+    <Routes>
+      <Route path="/" element={<AppLayout />}>
+        <Route index element={<HomePage />} />
+      </Route>
+
+      <Route element={<PrivatePage />}>
+        <Route path="/admin" element={<AdminPage />} />
+      </Route>
+
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
+  );
 }
 
 export default App;
